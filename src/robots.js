1f7f66f970554b1708060f4653e8607230a8c50f
Prakash Selvaraju <pselvaraju@changehealthcare.com>
2019-11-14 01:46:17 +0530

    First Commit

---
+++ src/robots.js
@@ -0,0 +1,50 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Inba',
+        username: 'Prakash',
+        email: 'prakash.sel@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Anbu',
+        username: 'Anbu',
+        email: 'anbu@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Magil',
+        username: 'Magil',
+        email: 'Magil@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Migalini Prakash',
+        username: 'Migal',
+        email: 'Migalini@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'AnbuInba',
+        username: 'Prakash',
+        email: 'prakash.sel@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'InbaAnbu',
+        username: 'Anbu',
+        email: 'anbu@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'MagilMathi',
+        username: 'Magil',
+        email: 'Magil@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Migal',
+        username: 'Migal',
+        email: 'Migalini@gmail.com'
+    }
+]
