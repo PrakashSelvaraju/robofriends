020027f45f59d0701957352f6c886a1fc3d3a9ca
Prakash Selvaraju <pselvaraju@changehealthcare.com>
2019-11-14 02:04:26 +0530

    Updates

---
+++ precache-manifest.ca9372f9f82c79f7b581d6f492b7fe99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaa6f912a956f3e84ec2856adaefb9e7",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "6f6d9243dc53ff0713d5",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "60ab827555b3668e69b3",
+    "url": "/robofriends/static/css/main.e0c3d22f.chunk.css"
+  },
+  {
+    "revision": "6f6d9243dc53ff0713d5",
+    "url": "/robofriends/static/js/2.2d717d87.chunk.js"
+  },
+  {
+    "revision": "60ab827555b3668e69b3",
+    "url": "/robofriends/static/js/main.08fcdf43.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  }
+]);
